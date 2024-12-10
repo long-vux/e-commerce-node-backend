de33bf1ac4f8f69c2756c64b8c684b4609cdd2c0
long-vux <hoanglongvu233@gmail.com>
2024-12-10 14:57:04 +0700

    add admin func

--- routes/coupon.js
+++ routes/coupon.js
@@ -6,7 +6,7 @@ const { auth, admin } = require('../middleware/authMiddleware');
 router.post('/create', auth, admin, CouponController.createCoupon);
 router.put('/update/:id', auth, admin, CouponController.updateCoupon);
 router.get('/getById/:id', auth, CouponController.getById);
-router.get('/getAll', auth, CouponController.getAll);
+router.get('/getAll', CouponController.getAll);
 router.delete('/delete/:id', auth, admin, CouponController.deleteCoupon);
 
 module.exports = router;
